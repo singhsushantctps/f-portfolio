b2f0e5f3fc78e3a2ff7c0f20a4c9dc6ad14c0be2
sushant kumar singh <singhsushantctps@gmail.com>
2022-01-30 16:03:57 +0530

    about page

--- ab.js
+++ ab.js
@@ -1,21 +1,22 @@
-const text =["proud Indian","web developer","Cricket lover"];
+const texti = ["a journey","LIFE!!","hard","easy","fun","printing Hello World !"];
 
-let count = 0;
-let index = 0;
-let currentText =" ";
-let letter = " ";
+let c = 0;
+let i = 0;
+let ct = " ";
+let l = " ";
 
-(function types(){
-  if(count === text.length){
-    count =0;
+(function typo(){
+  if(c===texti.length){
+    c=0;
   }
-  currentText = text[count];
-  letter = currentText.slice(0,++index);
+  ct = texti[c];
+  l= ct.slice(0,++i);
+  document.querySelector("#b").textContent=l;
 
-  document.querySelector("#b").textContent = letter;
-  if(letter.length === currentText.length){
-    count++;
-    index=0;
+  if(l.length===ct.length){
+    c++;
+    i=0;
+  
   }
-  setTimeout(types,350);
+  setTimeout(typo,600);
 })();
