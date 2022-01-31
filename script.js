ec8615c2fbef54930aa8ce7815d243a017c0c5b8
sushant kumar singh <singhsushantctps@gmail.com>
2022-01-31 20:39:26 +0530

    C:/Program Files/Git/

--- script.js
+++ script.js
@@ -21,6 +21,31 @@ let letter = " ";
   setTimeout(type,350);
 })();
 
+// about script
+
+const texti = ["a journey","LIFE!!","hard","easy","fun","printing Hello World !"];
+
+let c = 0;
+let i = 0;
+let ct = " ";
+let l = " ";
+
+(function typo(){
+  if(c===texti.length){
+    c=0;
+  }
+  ct = texti[c];
+  l= ct.slice(0,++i);
+  document.querySelector("#b").textContent=l;
+
+  if(l.length===ct.length){
+    c++;
+    i=0;
+  
+  }
+  setTimeout(typo,600);
+})();
+
 
 
 
